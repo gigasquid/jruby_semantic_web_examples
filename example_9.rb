f7947067eaacf070f92c610b682693b203bf554b
gigasquid <cmeier@gigasquidsoftware.com>
2011-07-06 22:59:33 -0400

    Cleaning up example 9

--- example_9.rb
+++ example_9.rb
@@ -1,19 +1,17 @@
 require 'javalib/jena-2.6.4.jar'
-require 'javalib/arq-2.8.7.jar'
 require 'javalib/icu4j-3.4.4.jar'
 require 'javalib/iri-0.8.jar'
 require 'javalib/log4j-1.2.13.jar'
-require 'javalib/lucene-core-2.3.1.jar'
 require 'javalib/slf4j-api-1.5.8.jar'
 require 'javalib/slf4j-log4j12-1.5.8.jar'
-require 'javalib/stax-api-1.0.1.jar'
-require 'javalib/wstx-asl-3.2.9.jar'
 require 'javalib/xercesImpl-2.7.1.jar'
 require 'java'
 
 java_import 'com.hp.hpl.jena.rdf.model.ModelFactory'
 java_import 'com.hp.hpl.jena.util.FileManager'
 java_import 'java.io.InputStream'
+
+#Tutorial 9 - demonstrate graph operations
 
 file_name_1 = "vc-db-3.rdf"   
 file_name_2 = "vc-db-4.rdf"
