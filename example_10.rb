8ad865cf4bc738205631aa5bc9ccc7f1bf763e16
gigasquid <cmeier@gigasquidsoftware.com>
2011-07-06 23:06:55 -0400

    Cleaning up example 10

--- example_10.rb
+++ example_10.rb
@@ -1,13 +1,9 @@
 require 'javalib/jena-2.6.4.jar'
-require 'javalib/arq-2.8.7.jar'
 require 'javalib/icu4j-3.4.4.jar'
 require 'javalib/iri-0.8.jar'
 require 'javalib/log4j-1.2.13.jar'
-require 'javalib/lucene-core-2.3.1.jar'
 require 'javalib/slf4j-api-1.5.8.jar'
 require 'javalib/slf4j-log4j12-1.5.8.jar'
-require 'javalib/stax-api-1.0.1.jar'
-require 'javalib/wstx-asl-3.2.9.jar'
 require 'javalib/xercesImpl-2.7.1.jar'
 require 'java'
 
@@ -17,20 +13,22 @@ java_import 'com.hp.hpl.jena.util.FileManager'
 java_import 'com.hp.hpl.jena.vocabulary.VCARD'
 java_import 'java.io.InputStream'
 
+#Tutorial 10 - demonstrate a container
+
 #Creating a model 
-m = ModelFactory.create_default_model
+model = ModelFactory.create_default_model
 
 #Finding the input file using the Jena File Manager
 input_file = FileManager.get.open "sample_input.rdf"
 
 #read the RDF/XML file
-m.read(input_file, nil)
+model.read(input_file, nil)
 
 #select all the resources with a VCARD.FN property
-iter = m.list_resources_with_property VCARD::FN
+iter = model.list_resources_with_property VCARD::FN
 puts "The database contains vcards for:"
 while iter.has_next
-  puts iter.next_resource.get_required_property(VCARD::FN).get_string
+  puts iter.next_resource.get_required_property(VCARD::FN).string
 end
 
 #select all the resources with a VCARD.FN property whose value ends
@@ -38,7 +36,7 @@ end
 
 class MySelector < SimpleSelector
   def selects(statement)
-    if statement.get_string.include?("Smith")
+    if statement.string.include?("Smith")
       return true
     else
       return false
@@ -50,17 +48,17 @@ end
 selector = MySelector.new(nil, VCARD::FN, nil)
 
 #create a bag
-smiths = m.create_bag
+smiths = model.create_bag
 
-iter = m.list_statements(selector)
+iter = model.list_statements(selector)
 puts "When using a selector for Smith we get:"
 while iter.has_next
   #add the smith to the bag
-  smiths.add( iter.next_statement.get_subject)
+  smiths.add( iter.next_statement.subject)
 end
 
 puts "The whole model is "
-m.write(java.lang.System::out)
+model.write(java.lang.System::out)
 
 iter = smiths.iterator
 puts "The smiths in the bag collection are:"
